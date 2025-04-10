65cda33c0b6e04c02f16a5cb23e49ddbfc7ab92f
Tom Bloor <tom@tbsliver.me>
2025-04-11 00:23:54 +0100

    embed url or url

--- src/extension/clips-manager.ts
+++ src/extension/clips-manager.ts
@@ -14,7 +14,8 @@ export function getClipsManager(nodecg: BundleAPI, twitchClient: ApiClient) {
         for await (const clip of allClips) {
             newClipArray.push({
                 id: clip.id,
-                url: clip.thumbnailUrl.replace("-preview-480x272.jpg", ".mp4"),
+                url: clip.url, //clip.thumbnailUrl.replace("-preview-480x272.jpg", ".mp4"),
+                embed_url: clip.embedUrl,
                 created_at: clip.creationDate.toISOString(),
                 creator_name: clip.creatorDisplayName,
                 title: clip.title
--- src/extension/types-common.d.ts
+++ src/extension/types-common.d.ts
@@ -36,6 +36,7 @@ export interface TwitchHello {
 export interface TwitchClip {
     id: string;
     url: string;
+    embed_url: string;
     creator_name: string;
     title: string;
     created_at: string;
